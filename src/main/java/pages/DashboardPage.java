f16b2eea369b90563a4589b467c4c2839fc26b94
rohit_igurusol <rohit@igurusolutions.com>
2024-04-01 12:49:43 +0530

    browserstack_config_done file

--- src/main/java/pages/DashboardPage.java
+++ src/main/java/pages/DashboardPage.java
@@ -1,12 +1,9 @@
 package pages;
 import org.openqa.selenium.By;
 
-import static org.openqa.selenium.By.xpath;
-
 public class DashboardPage extends BasePage{
     By signOutBtn;
     public DashboardPage(){
         this.signOutBtn= By.xpath("//a[normalize-space()='Sign Out']");
-        System.out.println("hello 2");
     }
 }
--- src/main/java/utility/BrowserKeeper.java
+++ src/main/java/utility/BrowserKeeper.java
@@ -1,9 +1,11 @@
 package utility;
 
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
 
 public class BrowserKeeper { 
   private static WebDriver browser;
@@ -11,13 +13,23 @@ public class BrowserKeeper {
    public BrowserKeeper(){
 	   
    }
-   public void setupWebDriver() {
-	    DesiredCapabilities caps=new DesiredCapabilities();
-		ChromeOptions option=new ChromeOptions();	
-		option.addArguments("start-maximized");
-		option.merge(caps);
-		System.setProperty("webdriver.chrome.driver","src/main/resources/chromeDriver/chromedriver.exe");
-	    browser = new ChromeDriver(option);  
+   public void setupWebDriver() throws MalformedURLException {
+       String username = System.getenv("BROWSERSTACK_USERNAME");
+       String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+//       String username = "rohitkumar_ufv9J8";
+//       String accessKey = "FYLuo28MBhzbbAhQCXxz";
+       MutableCapabilities capabilities = new MutableCapabilities();
+       capabilities.setCapability("browserName", "Chrome");
+       HashMap<String, String> browserstackOptions = new HashMap<String, String>();
+       browserstackOptions.put("os", "Windows");
+       capabilities.setCapability("bstack:options", browserstackOptions);
+       browser = new RemoteWebDriver(new URL("https://" + username + ":" + accessKey + "@hub.browserstack.com/wd/hub"), capabilities);
+//        DesiredCapabilities caps=new DesiredCapabilities();
+//		ChromeOptions option=new ChromeOptions();
+//		option.addArguments("start-maximized");
+//		option.merge(caps);
+//		System.setProperty("webdriver.chrome.driver","src/main/resources/chromeDriver/chromedriver.exe");
+//	    browser = new ChromeDriver(option);
    }
    
    public  WebDriver getBrowserInstance() {
